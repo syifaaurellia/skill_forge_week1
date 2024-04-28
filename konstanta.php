5e9056561e08541925249ce6f959747ba90f54da
Syifaaurellia18 <syifarrahma2@gmail.com>
2024-04-28 12:57:02 +0700

    project 1

---
+++ konstanta.php
@@ -0,0 +1,16 @@
+<?php
+
+define('panjang', 10);
+
+echo "nilai konstanta panjang : " . panjang;
+
+// echo "nilai konstanta panjang : " . panjang;
+
+// konstanta dengan kata const
+ const nilaiPanjang = 5;
+
+ echo "nilai konstanta panjang: " . nilaiPanjang;
+
+/**
+ * fungsi untuk menarik data mahasiswa dari tabel mahasiswa
+ * 
